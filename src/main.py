2347e82832f99249cb211e43108c2a8df6cb6cb0
dmosc <osdavrm@gmail.com>
2022-11-08 21:34:37 -0600

    Add fixes to scoring function and update print_board function.

--- src/main.py
+++ src/main.py
@@ -4,51 +4,55 @@
 import math
 
 
-BOARD_LENGTH = 3
+BOARD_SIZE = 3
 player, opponent = 'x', 'o'
 
 
 def print_board(board: list[list[int]]):
-  for i in range(BOARD_LENGTH):
-    print(i, board[i])
+  for i in range(BOARD_SIZE):
+    print('-------------------')
+    print('|     |     |     |')
+    print('| ', board[i][0], ' | ', board[i][1], ' | ', board[i][2], ' |')
+    print('|     |     |     |')
+  print('-------------------')
 
 
 def is_move_available(board: list[list[int]]):
-  for i in range(BOARD_LENGTH):
-    for j in range(BOARD_LENGTH):
+  for i in range(BOARD_SIZE):
+    for j in range(BOARD_SIZE):
       if board[i][j] == '-':
         return True
   return False
 
 
 def board_score(board: list[list[int]]):
-  for i in range(BOARD_LENGTH):
+  for i in range(BOARD_SIZE):
     # Find winning rows.
     if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
       if board[i][0] == player:
-        return 1
+        return 10
       elif board[i][0] == opponent:
-        return -1
+        return -10
 
     # Find winning columns.
     if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
       if board[0][i] == player:
-        return 1
+        return 10
       elif board[0][i] == opponent:
-        return -1
+        return -10
 
   # Find winning diagonals.
-  if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) :
-    if (board[0][0] == player) :
-      return 1
-    elif (board[0][0] == opponent) :
-      return -1
+  if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+    if board[0][0] == player:
+      return 10
+    elif board[0][0] == opponent:
+      return -10
 
-  if (board[0][2] == board[1][1] and board[1][1] == board[2][0]) :
-    if (board[0][2] == player) :
-      return 1
-    elif (board[0][2] == opponent) :
-      return -1
+  if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+    if board[0][2] == player:
+      return 10
+    elif board[0][2] == opponent:
+      return -10
 
   return 0
 
@@ -62,24 +66,23 @@ def minimax(board: list[list[int]], is_max_turn: bool):
 
   token = player if is_max_turn else opponent
   comp = max if is_max_turn else min
-  for i in range(BOARD_LENGTH):
-    for j in range(BOARD_LENGTH):
+  for i in range(BOARD_SIZE):
+    for j in range(BOARD_SIZE):
       if board[i][j] == '-':
         board[i][j] = token
         best = comp(best, minimax(board, not is_max_turn))
         board[i][j] = '-'
-
   return best
 
 
 def best_move(board: list[list[int]]):
   best = math.inf
   best_move = (-1, -1)
-  for i in range(BOARD_LENGTH):
-    for j in range(BOARD_LENGTH):
+  for i in range(BOARD_SIZE):
+    for j in range(BOARD_SIZE):
       if board[i][j] == '-':
         board[i][j] = opponent
-        move = min(best, minimax(board, True))
+        move = minimax(board, True)
         board[i][j] = '-'
         if move < best:
           best = move
@@ -95,13 +98,21 @@ def main():
   ]
   while is_move_available(board) and board_score(board) == 0:
     print_board(board)
+    print('\n- Input your next move -')
     i, j = int(input('row [0, 2]: ')), int(input('col [0, 2]: '))
     if (board[i][j] == '-'):
       board[i][j] = player
-      opponent_move = best_move(board)
-      board[opponent_move[0]][opponent_move[1]] = opponent
+      if (not board_score(board)):
+        opponent_move = best_move(board)
+        board[opponent_move[0]][opponent_move[1]] = opponent
   print_board(board)
-  print(board_score(board))
+  score = board_score(board)
+  if (score > 0):
+    print('\nYou have won!')
+  elif (score < 0):
+    print('\nThe opponent has won!')
+  else:
+    print('\nThere has been a tie!')
 
 if __name__ == '__main__':
   main()
